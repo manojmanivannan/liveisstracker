2235915c69ded75353b369d721dd94ab3fae07e4
Manoj Manivannan <mmanivannan@empirix.com>
2020-12-21 20:11:07 +0100

    Use tini to control process, entrypoint.sh to start the application

--- src/main/resources/entrypoint.sh
+++ src/main/resources/entrypoint.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-echo "Entry point script was run"
+#!/usr/bin/env python
+
+import sys, os
+from streamlit import cli as stcli
+
+if __name__ == '__main__':
+    os.chdir('/home/manoj/${python_package}/')
+    sys.argv = ["streamlit", "run", "TrackISS.py"]
+    sys.exit(stcli.main())
+
